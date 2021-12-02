c9259c6834371ffba9bc4e7b3f8e0e6455102373
1902667-JacksonWong <1902667@sit.singaporetech.edu.sg>
2021-12-02 14:56:10 +0800

    s

--- lab/app.py
+++ lab/app.py
@@ -12,12 +12,17 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == "GET":
+    # if request.method == "GET":
     #     return f"Request Recieved"
+     
+    if request.method == "POST":
+        input1 = request.form['search']
         print('hello')
+        print(input1)
     return render_template('index.html')
 
 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port = 80)
+    #app.run(debug=True, host='0.0.0.0', port = 8080)
